6b9a3008c5e82e920bf2e3d9f8c31c970bbe000c
Juny Kesumadewi <juny.kesumadewi@unimelb.edu.au>
2020-11-26 19:49:04 +1100

    check status in a loop

--- toolbuilder/runtest/check_test_status.py
+++ toolbuilder/runtest/check_test_status.py
@@ -1,7 +1,35 @@
 import sys
+import time
+import json
 
-def check_test_status(run_id: str):
-    print(run_id)
+
+def check_test_status(url: str):
+    import requests
+
+    status = None
+    keep_checking = True
+    max_try = 3
+    count = 0
+    while keep_checking:
+        count += 1
+        resp = requests.get(url)
+        if resp.status_code == requests.codes.ok:
+            as_json = resp.json()
+            print(as_json)
+            if as_json["data"]["status"] in ["failed", "succeeded"]:
+                status = as_json["data"]["status"]
+                keep_checking = False
+
+        if count >= max_try:
+            keep_checking = False
+
+        time.sleep(10)
+
+    if status is None:
+        raise Exception("Failed to fetch test status")
+
+    if status != "succeeded":
+        raise Exception("Test failed")
 
 
 if __name__ == "__main__":
